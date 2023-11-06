e82d41df97b9113a44f90575e03077a055505e1d
Pranavkoti <kpranav@wustl.edu>
2023-11-05 22:03:40 -0600

    Change 3

--- view.js
+++ view.js
@@ -13,7 +13,7 @@ function make_file_element(file_obj) {
             <h3 id="${file_hash}_header">
                 <span class="oi oi-folder" id="${file_hash}_icon"/> ${file_obj.filename} 
                 <button class="ui-button ui-widget ui-corner-all permbutton" path="${file_hash}" id="${file_hash}_permbutton"> 
-                    <span class="oi oi-lock-unlocked" id="${file_hash}_permicon"> Change Permissions</span> 
+                    <span class="oi oi-lock-unlocked" id="${file_hash}_permicon"> Click Here to Change Permissions</span> 
                 </button>
             </h3>
         </div>`)
@@ -33,7 +33,7 @@ function make_file_element(file_obj) {
         return $(`<div class='file'  id="${file_hash}_div">
             <span class="oi oi-file" id="${file_hash}_icon"/> ${file_obj.filename}
             <button class="ui-button ui-widget ui-corner-all permbutton" path="${file_hash}" id="${file_hash}_permbutton"> 
-            <span class="oi oi-lock-unlocked" id="${file_hash}_permicon"> Change Permissions</span> 
+            <span class="oi oi-lock-unlocked" id="${file_hash}_permicon"> Click Here to Change Permissions</span> 
             </button>
         </div>`)
     }
